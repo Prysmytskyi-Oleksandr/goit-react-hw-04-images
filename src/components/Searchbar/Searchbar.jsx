c7785397022d0897b58761d4506555ad1c675872
Prysmytskyi-Oleksandr <san2003@ukr.net>
2023-02-07 15:11:28 +0200

    start

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,51 +1,92 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './searchbar.module.css';
 
-export class Searchbar extends Component {
-  state = {
-    searchName: '',
+export const Searchbar = ({ onSubmit }) => {
+  const [searchName, setSearchName] = useState('');
+
+  const handleNameChange = event => {
+    setSearchName(event.currentTarget.value.toLowerCase());
   };
 
-  handleNameChange = event => {
-    this.setState({ searchName: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    const { searchName } = this.state;
     if (searchName.trim() === '') {
       alert('Введіть запит');
       return;
     }
-    this.props.onSubmit(searchName);
-    this.setState({ searchName: '' });
+    onSubmit(searchName);
+    setSearchName('');
   };
 
-  render() {
-    const { searchName } = this.state;
-    return (
-      <header className={styles.searchbar}>
-        <form onSubmit={this.handleSubmit} className={styles.form}>
-          <button type="submit" className={styles.button}></button>
+  return (
+    <header className={styles.searchbar}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <button type="submit" className={styles.button}></button>
 
-          <input
-            className={styles.input}
-            type="text"
-            autocomplete="off"
-            autofocus
-            placeholder="Search images and photos"
-            name="searchName"
-            value={searchName}
-            onChange={this.handleNameChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={styles.input}
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+          name="searchName"
+          value={searchName}
+          onChange={handleNameChange}
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+// export class Searchbar extends Component {
+//   state = {
+//     searchName: '',
+//   };
+
+//   handleNameChange = event => {
+//     this.setState({ searchName: event.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     const { searchName } = this.state;
+//     if (searchName.trim() === '') {
+//       alert('Введіть запит');
+//       return;
+//     }
+//     this.props.onSubmit(searchName);
+//     this.setState({ searchName: '' });
+//   };
+
+//   render() {
+//     const { searchName } = this.state;
+//     return (
+//       <header className={styles.searchbar}>
+//         <form onSubmit={this.handleSubmit} className={styles.form}>
+//           <button type="submit" className={styles.button}></button>
+
+//           <input
+//             className={styles.input}
+//             type="text"
+//             autocomplete="off"
+//             autofocus
+//             placeholder="Search images and photos"
+//             name="searchName"
+//             value={searchName}
+//             onChange={this.handleNameChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// Searchbar.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
